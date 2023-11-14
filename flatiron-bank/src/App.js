f3989e9c83ed7d343d28f03cf8515db3c0a61a72
philipwanjiku <muhorophilip7@gmail.com>
2023-11-14 21:41:06 +0300

    Transaction Form

--- flatiron-bank/src/App.js
+++ flatiron-bank/src/App.js
@@ -4,7 +4,7 @@ import TransactionList from "./TransactionList";
 
 
 const App = () => {
-
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     // Fetch data from the local json server
@@ -16,6 +16,13 @@ const App = () => {
       })
       .catch(error => console.error('Error fetching data:', error));
     }, []);  //loops continiously hence the array so as to run once
+
+    const addTransaction = (newTransaction) => {
+      // Simulate adding a new transaction locally
+      const updatedTransactions = [...transactions, { id: transactions.length + 1, ...newTransaction }];
+      setTransactions(updatedTransactions);
+      setFilteredTransactions(updatedTransactions);
+    };
   
 
   return(
--- flatiron-bank/src/TransactionList.js
+++ flatiron-bank/src/TransactionList.js
@@ -1,5 +1,23 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// const TransactionList = ({ transactions, onAddTransaction, onFilterTransactions }) => {
-//   const [newTransaction, setNewTransaction] = useState({ description: '', amount: '' });
-//   const [searchTerm, setSearchTerm] = useState('');
+const TransactionList = ({ transactions, onAddTransaction, onFilterTransactions }) => {
+  const [newTransaction, setNewTransaction] = useState({ description: '', amount: '' });
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleAddTransaction = (e) => {
+    e.preventDefault();
+    onAddTransaction(newTransaction);
+    setNewTransaction({ description: '', amount: '' });
+  };
+  return (
+    <div>
+        <form onSubmit={handleAddTransaction}>
+        <label>
+          Description:
+          <input
+            type="text"
+            name="description"
+            value={newTransaction.description}
+            onChange={handleInputChange}
+          />
+     </label>
