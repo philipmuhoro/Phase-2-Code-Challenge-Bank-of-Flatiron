106765507b4512b3d5e2c60bd12a2c73ff7de562
philipwanjiku <muhorophilip7@gmail.com>
2023-11-14 21:30:44 +0300

    Fetch data

--- flatiron-bank/src/App.js
+++ flatiron-bank/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Header from "./Header";
+import TransactionList from "./TransactionList";
 
 
-function App () {
+const App = () => {
 
+
+  useEffect(() => {
+    // Fetch data from the local json server
+    fetch('http://localhost:3000/transactions')
+      .then(response => response.json())
+      .then(data => {
+        setTransactions(data);
+        setFilteredTransactions(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+    }, []);  //loops continiously hence the array so as to run once
   
 
   return(
     <div>
       <Header />
+      <TransactionList />
       
     
       
---
+++ flatiron-bank/src/TransactionList.js
@@ -0,0 +1,5 @@
+// import React, { useState } from 'react';
+
+// const TransactionList = ({ transactions, onAddTransaction, onFilterTransactions }) => {
+//   const [newTransaction, setNewTransaction] = useState({ description: '', amount: '' });
+//   const [searchTerm, setSearchTerm] = useState('');
